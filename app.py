0ee4b3faccc5c2080b9eb239877df9786435a072
pallaviferrao <pallaviferrao2004@gmail.com>
2023-06-29 10:43:35 +0530

    Changing the response

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Flask by pallavi2!"
+    return jsonify({'prediction': 'What is Inkita doing?', 'value': 'She will let me know'})
